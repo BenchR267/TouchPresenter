aede2ab6faa98715ca722bc4c0193a8af1790fea
Benjamin Herzog <mail@benchr.de>
2016-07-24 11:06:21 +0200

    initializer of configuration should not be iOS 9 only. Fixed also introduction in readme.

--- TouchPresenter/TPWindow.swift
+++ TouchPresenter/TPWindow.swift
@@ -11,7 +11,7 @@ import UIKit
 private var IndicatorObjectKey: UInt8 = 0
 
 internal extension UITouch {
-    
+
     /// Hold a strong reference to the view for removing it later
     var indicator: UIView? {
         get {
@@ -24,25 +24,24 @@ internal extension UITouch {
 }
 
 public struct TouchPresenterConfiguration<ViewType: UIView> {
-    
+
     /// Size of the view
     let size: CGSize
-    
+
     /// true if 3d touch should be visualized
     let threeDeeTouchEnabled: Bool
-    
+
     /**
      Initialize a configuration instance.
-     
+
      - parameter viewType:          the type of the view that should be used
      - parameter size:              the size of the view
      - parameter forceTouchEnabled: use forcetouch
-     
+
      *NOTE* size is default at 50x50 points
-     
+
      *NOTE* forceTouchEnabled is only available on iOS 9 or greater on appropriate devices
      */
-    @available(iOS 9.0, *)
     public init(viewType: ViewType.Type, size: CGSize = CGSize(width: 50, height: 50), enable3DTouch: Bool = false) {
         self.size = size
         self.threeDeeTouchEnabled = enable3DTouch
@@ -52,15 +51,15 @@ public struct TouchPresenterConfiguration<ViewType: UIView> {
 
 /**
  Use this window subclass as your main window, it will catch all touches and highlight them with the given view class.
- 
+
  Initialize in the following way:
- 
+
  ```Swift
  TPWindow(frame: UIScreen.mainScreen().bounds, configuration: TouchPresenterConfiguration(viewType: TPLightBlueCircleIndicator.self))
  ```
- 
+
  You can just override your init method in AppDelegate like the following:
- 
+
  ```
  override init() {
  let config = TouchPresenterConfiguration(viewType: TPLightBlueCircleIndicator.self, enable3DTouch: true)
@@ -68,13 +67,13 @@ public struct TouchPresenterConfiguration<ViewType: UIView> {
  super.init()
  }
  ```
- 
+
  You can set any subclass of UIView as the viewType. The window will then initialize an instance of it in the given size.
  */
 public class TPWindow<ViewType: UIView>: UIWindow {
-    
+
     private let configuration: TouchPresenterConfiguration<ViewType>
-    
+
     /**
      Initializes an instance of the window. See class documentation for more details.
      */
@@ -82,15 +81,15 @@ public class TPWindow<ViewType: UIView>: UIWindow {
         self.configuration = configuration
         super.init(frame: frame)
     }
-    
+
     public override func sendEvent(event: UIEvent) {
         super.sendEvent(event)
-        
+
         event.allTouches()?.forEach {
             touch in
-            
+
             switch touch.phase {
-                
+
             case .Began:
                 let touchPosition = touch.locationInView(self)
                 let origin = CGPoint(x: touchPosition.x - configuration.size.width/2, y: touchPosition.y - configuration.size.height/2)
@@ -98,27 +97,27 @@ public class TPWindow<ViewType: UIView>: UIWindow {
                 let indicator = ViewType(frame: frame)
                 touch.indicator = indicator
                 addSubview(indicator)
-                
+
             case .Stationary:
                 break
-                
+
             case .Moved:
                 touch.indicator?.center = touch.locationInView(self)
-                
+
             case .Ended, .Cancelled:
                 touch.indicator?.removeFromSuperview()
                 touch.indicator = nil
             }
-            
+
             if configuration.threeDeeTouchEnabled {
                 if #available(iOS 9.0, *) {
-                    
+
                     let calculatedForce = max(1, sqrt(touch.force))
                     touch.indicator?.transform = CGAffineTransformMakeScale(calculatedForce, calculatedForce)
                 }
             }
-            
+
         }
     }
-    
+
 }
